daef6a7c9d815a25a2dfefd20475cc2ee7c9cafd
marianmadalin <marianmadalin01@gmail.com>
2021-04-08 21:33:39 +0300

    oleaca factorizare + stl la ownerName pentru ca citire

--- 144-Neagu-Marian-Proiect2-Tema12.cpp
+++ 144-Neagu-Marian-Proiect2-Tema12.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 
 #pragma region Clase
 class Card
 {
-    char* m_cardNr; // Cele 16 cifre
-    char* m_ownerName; // NUME PRENUME
+    std::string m_ownerName; // NUME PRENUME
+    char* m_cardNr; // de obicei 16 cifre
     char* m_expirationDate; // DD/LL/AAAA
     int m_cvv; // 3 cifre
     double m_credit; // suma in RON
 
     public:
     // Constructori
-    Card(): m_ownerName(nullptr), m_cardNr(new char[16]), m_expirationDate(new char[10]) {}
+    Card(): m_ownerName(NULL), m_cardNr(nullptr), m_expirationDate(nullptr), m_cvv(-1), m_credit(-1) {}
 
-    Card(const char* cardNr, const char* ownerName, const char* expirationDate, int cvv, double credit);
+    Card(std::string ownerName, char* cardNr, const char* expirationDate, int cvv, double credit);
 
 
     //Constructor de copiere
@@ -22,11 +23,10 @@ class Card
     Card(const Card &card)
     {
         m_cardNr = new char[strlen(card.m_cardNr)];
-        m_ownerName = new char[strlen(card.m_ownerName)];
         m_expirationDate = new char[strlen(card.m_expirationDate)];
 
         strcpy(m_cardNr, card.m_cardNr);
-        strcpy(m_ownerName, card.m_ownerName);
+        m_ownerName = card.m_ownerName;
         strcpy(m_expirationDate, card.m_expirationDate);
         m_cvv = card.m_cvv;
         m_credit = card.m_credit;
@@ -35,8 +35,6 @@ class Card
     // Destructor
     ~Card()
     {
-        delete[] m_ownerName;
-        m_ownerName = nullptr;
         delete[] m_cardNr;
         m_cardNr = nullptr;
         delete[] m_expirationDate;
@@ -55,7 +53,29 @@ class Card
     void SetExpirationDate(const char* expirationDate);
     void SetCVV(int cvv);
     void SetCredit(double credit);
+
     // Operatori
+    Card& operator= (const Card &card)
+    {
+        // dezalocare memorie 
+        delete[] m_cardNr;
+        delete[] m_expirationDate;
+
+        m_expirationDate = new char[10];
+        m_cardNr = new char[strlen(card.m_cardNr)];
+
+        // atribuire valori noi
+        strcpy(m_expirationDate, card.m_expirationDate);
+        strcpy(m_cardNr, card.m_cardNr);
+        m_ownerName = card.m_ownerName;
+
+        m_cvv = card.m_cvv;
+        m_credit = card.m_credit;
+
+        return *this;
+    }
+    friend std::istream& operator>> (std::istream& in,  Card& card);
+    friend std::ostream& operator<< (std::ostream& out,  Card& card);
 
     // Metode
 
@@ -75,14 +95,13 @@ class CardPremium: public CardStandard
 #pragma region Constructori
 
 // CARD
-Card::Card(const char* cardNr, const char* ownerName, const char* expirationDate, int cvv, double credit)
+Card::Card(std::string ownerName, char* cardNr, const char* expirationDate, int cvv, double credit)
 {
     m_cardNr = new char[strlen(cardNr)];
-    m_ownerName = new char[strlen(cardNr)];
     m_expirationDate = new char[strlen(cardNr)];
 
     strcpy(m_cardNr, cardNr);
-    strcpy(m_ownerName, ownerName);
+    m_ownerName = ownerName;
     strcpy(m_expirationDate, expirationDate);
     m_cvv = cvv;
     m_credit = credit;
@@ -110,6 +129,7 @@ int Card::GetCVV()
 }
 double Card::GetCredit()
 {
+
     return m_credit;
 }
 
@@ -137,6 +157,67 @@ void Card::SetCredit(double credit)
 }
 #pragma endregion
 
+#pragma region Operatori
+
+std::istream& operator>> (std::istream& in,  Card& card)
+{
+	// delete[] multime.m_set;
+	// multime.m_numberOfNumbers = 0;
+	// multime.m_set = nullptr;
+    
+    // dezalocare
+    delete[] card.m_cardNr;
+    delete[] card.m_expirationDate;
+
+    card.m_cardNr = nullptr;
+    card.m_ownerName = nullptr;
+    card.m_expirationDate = nullptr;
+
+    card.m_cvv = -1;
+    card.m_credit = -1;
+
+    std::string name;
+    char* cardNr = nullptr; 
+    char* expDate = nullptr;
+
+    std::cout<<"~Introduceti datele~"<<std::endl;
+    std::cout<<"Numele detinatorului: ";
+    in>>name;
+    card.m_ownerName = name;
+
+    std::cout<<"Numerele cardului: ";
+    in>>cardNr;
+    card.m_cardNr = new char[strlen(cardNr)];
+    strcpy(card.m_cardNr, cardNr);
+
+    std::cout<<"Data expirarii: ";
+    in>>expDate;
+    card.m_expirationDate = new char[strlen(expDate)];
+    strcpy(card.m_expirationDate, expDate);
+
+    std::cout<<"CVV: ";
+    in>>card.m_cvv;
+
+    std::cout<<"Credit: ";
+    in>>card.m_credit;
+
+
+	// multime.m_set = new int[multime.m_numberOfNumbers];
+    // for(int i = 0; i < multime.m_numberOfNumbers; i++)
+	// 	in>>multime.m_set[i];
+    
+
+	return in;
+}
+
+std::ostream& operator<< (std::ostream& out, const Card& card)
+{
+	
+
+	return out;
+}
+
+#pragma endregion
 // CARD
 void ShowDetails_Demo(Card card)
 {
@@ -154,17 +235,26 @@ void ShowDetails_Demo(Card card)
 
 void Demo()
 {
-    Card card("9837983798379837", "POPESCU ION SIMION", "12/10/2023", 453, 0.0);
-    Card cardCopy(card);
+    //Card card("9837983798379837", "POPESCU ION SIMION", "12/10/2023", 453, 0.0);
+    Card card2;
 
-    ShowDetails_Demo(card);
-    ShowDetails_Demo(cardCopy);
+    std::cin>>card2;
+
+    //ShowDetails_Demo(card);
+    ShowDetails_Demo(card2);
 
 }
 
+void test()
+{
+    std::string test;
+    std::cout<<"Test: ";
+    std::cin>>test;
+    std::cout<<test;
+}
 int main()
 {
-
-    Demo();
+    test();
+    //Demo();
     return 0;
 }
